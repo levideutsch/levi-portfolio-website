fbc99361b0e256cc1e9e69dd5401b13cfa41beff
levideutsch <levideutsch@live.com>
2024-07-31 16:01:54 -0400

    added mui and mui box to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Card from "@mui/material/Card";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{textAlign: "center"}}>
       
       levi deutsch portfilio website
       this is for the second commit 
       im adding a 3rd line to see how it looks
+      <Card style={{width: "50%", height: "50%", marginTop: "40px", backgroundColor: "red", margin: "0 auto"}}>
+        this is my first card
+      </Card>
     </div>
   );
 }
