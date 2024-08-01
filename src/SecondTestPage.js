5274693025ad2b199e97306f48aee3b5e0bc1ac7
levideutsch <levideutsch@live.com>
2024-07-31 22:21:15 -0400

    click button test

--- src/SecondTestPage.js
+++ src/SecondTestPage.js
@@ -1,9 +1,18 @@
+import React, { useState } from "react"
+
 function SecondTestPage() {
+    const [isClicked, setIsClicked] = useState(false)
+
+
     return (
         <div>
             second test page
             adding new data to main branch
             adding MORE new data 
+            <button onClick={() => setIsClicked(!isClicked)}>
+                {isClicked ? "close" : "open"}
+            </button>
+
         </div>
     )
 }
